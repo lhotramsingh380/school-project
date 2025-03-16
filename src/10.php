9c23e8822ad53ca1b2ece97ac741e3511d15d532
lhotramsingh380 <lhotramsingh380@gmail.com>
2025-03-16 12:48:07 +0000

    Add 10.php

---
+++ src/10.php
@@ -0,0 +1,22 @@
+<?php
+class Person {
+    private $name;
+    private $age;
+
+    public function __construct($name, $age) {
+        $this->name = $name;
+        $this->age = $age;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function getAge() {
+        return $this->age;
+    }
+}
+
+$person = new Person('John Doe', 30);
+echo $person->getName() . ' is ' . $person->getAge() . ' years old.';
+?>
