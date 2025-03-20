51aed210ddea3e663e6e941cd06f2f0a8cf74138
lhotramsingh380 <lhotramsingh380@gmail.com>
2025-03-20 17:09:30 +0000

    Add 14.php

---
+++ src/14.php
@@ -0,0 +1,7 @@
+<?php
+// Example PHP code to demonstrate basic file operations
+$filePath = "path/to/your/file.txt";
+file_put_contents($filePath, "Hello, World!"); // Write data to a file
+
+echo "Data has been written successfully.";
+?>
